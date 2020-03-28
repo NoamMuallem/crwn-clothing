4f180ef3a1dd019a0e508ca47664f6158de7c58a
NoamMuallem <noammu072@gmail.com>
2020-03-28 11:48:47 +0300

    updating the app with a shop page, collection preview component and collection item component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+
+const CollectionPreview = ({ title, items, routeName }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {/* we wont to preview only 4 items, so we will filter first and then map them */}
+        {items
+          .filter((item, index) => index < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
